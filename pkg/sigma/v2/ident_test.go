3c424eb76904698ae631df6911f605936a2fe1f8
Markus Kont <markus.kont@ccdcoe.org>
2020-04-27 15:25:37 +0300

    Fix test cases
    tabs v spaces

--- pkg/sigma/v2/ident_test.go
+++ pkg/sigma/v2/ident_test.go
@@ -1,5 +1,11 @@
 package sigma
 
+import (
+	"testing"
+
+	"gopkg.in/yaml.v2"
+)
+
 var identCase1 = `
 ---
 detection:
@@ -26,25 +32,36 @@ detection:
   condition: selection1
   selection1:
     winlog.event_data.ScriptBlockText:
-    - ' -FromBase64String'
+    - " -FromBase64String"
     task: "Execute a Remote Command"
 `
 
 var identCase4 = `
 ---
 detection:
-	condition: keywords
-	keywords:
-		- 'wget * - http* | perl'
-		- 'wget * - http* | sh'
-		- 'wget * - http* | bash'
-		- 'python -m SimpleHTTPServer'
+  condition: keywords
+  keywords:
+  - 'wget * - http* | perl'
+  - 'wget * - http* | sh'
+  - 'wget * - http* | bash'
+  - 'python -m SimpleHTTPServer'
 `
 var identCase5 = `
 ---
 detection:
-	condition: selection
-	selection:
-		CommandLine|endswith: '.exe -S'
-		ParentImage|endswith: '\services.exe'
+  condition: selection
+  selection:
+  CommandLine|endswith: '.exe -S'
+  ParentImage|endswith: '\services.exe'
 `
+
+var identCases = []string{identCase1, identCase2, identCase3, identCase4, identCase5}
+
+func TestParseIdent(t *testing.T) {
+	for i, c := range identCases {
+		var r RuleHandle
+		if err := yaml.Unmarshal([]byte(c), &r); err != nil {
+			t.Fatalf("ident case %d yaml parse fail: %s", i+1, err)
+		}
+	}
+}
