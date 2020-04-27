016754d7ad3e52ea52534e9d277c8e3850a9e154
Markus Kont <markus.kont@ccdcoe.org>
2020-04-27 16:49:21 +0300

    WIP: Identifier parsing

--- pkg/sigma/v2/ident.go
+++ pkg/sigma/v2/ident.go
@@ -1,1 +1,44 @@
 package sigma
+
+import "strings"
+
+type identType int
+
+func (i identType) String() string {
+	switch i {
+	case identKeyword:
+		return "KEYWORD"
+	case identSelection:
+		return "SELECTION"
+	default:
+		return "UNK"
+	}
+}
+
+const (
+	identErr identType = iota
+	identSelection
+	identKeyword
+)
+
+func checkIdentType(item Item, data interface{}) identType {
+	t := reflectIdentKind(data)
+	if strings.HasPrefix(item.Val, "keyword") {
+		if data == nil {
+			return identKeyword
+		}
+		if t != identKeyword {
+			return identErr
+		}
+	}
+	return t
+}
+
+func reflectIdentKind(data interface{}) identType {
+	switch data.(type) {
+	case map[string]interface{}, map[interface{}]interface{}:
+		return identSelection
+	default:
+		return identKeyword
+	}
+}
--- pkg/sigma/v2/ident_test.go
+++ pkg/sigma/v2/ident_test.go
@@ -1,7 +1,6 @@
 package sigma
 
 import (
-	"fmt"
 	"testing"
 
 	"gopkg.in/yaml.v2"
@@ -58,16 +57,17 @@ detection:
 
 type identTestCase struct {
 	IdentCount int
+	IdentTypes []identType
 	Rule       string
 	Pos, Neg   string
 }
 
 var identCases = []identTestCase{
-	{IdentCount: 1, Rule: identCase1},
-	{IdentCount: 2, Rule: identCase2},
-	{IdentCount: 1, Rule: identCase3},
-	{IdentCount: 1, Rule: identCase4},
-	{IdentCount: 1, Rule: identCase5},
+	{IdentCount: 1, Rule: identCase1, IdentTypes: []identType{identSelection}},
+	{IdentCount: 2, Rule: identCase2, IdentTypes: []identType{identSelection, identSelection}},
+	{IdentCount: 1, Rule: identCase3, IdentTypes: []identType{identSelection}},
+	{IdentCount: 1, Rule: identCase4, IdentTypes: []identType{identKeyword}},
+	{IdentCount: 1, Rule: identCase5, IdentTypes: []identType{identSelection}},
 }
 
 func TestParseIdent(t *testing.T) {
@@ -81,15 +81,25 @@ func TestParseIdent(t *testing.T) {
 			t.Fatalf("ident case %d missing condition", i+1)
 		}
 		l := lex(condition)
+		var items, j int
 		for item := range l.items {
 			switch item.T {
 			case TokIdentifier:
 				val, ok := r.Detection[item.Val]
 				if !ok {
-					t.Fatalf("ident case %d missing ident %s or unable to extract", i+0, item.Val)
+					t.Fatalf("ident case %d missing ident %s or unable to extract", i+1, item.Val)
 				}
-				fmt.Println(val)
+				items++
+				if k := checkIdentType(item, val); k != c.IdentTypes[j] {
+					t.Fatalf("ident case %d ident %d kind mismatch expected %s got %s",
+						i+1, j+1, c.IdentTypes[j], k)
+				}
+				j++
 			}
+		}
+		if items != c.IdentCount {
+			t.Fatalf("ident case %d defined element count %d does not match processd %d",
+				i+1, c.IdentCount, items)
 		}
 	}
 }
