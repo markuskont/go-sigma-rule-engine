349c135de10d298374022a29fba1eaff302a4ded
Markus Kont <markus.kont@ccdcoe.org>
2020-04-28 17:15:01 +0300

    Implement a simple atomic text matcher for patterns

--- pkg/sigma/v2/pattern.go
+++ pkg/sigma/v2/pattern.go
@@ -1,55 +1,74 @@
 package sigma
 
-import (
-	"regexp"
-	"strings"
-
-	"github.com/ryanuber/go-glob"
-)
+import "strings"
 
 type TextPatternType int
 
-const (
-	PatLiteral TextPatternType = iota
-	PatPrefix
-	PatSuffix
-	PatGlob
-	PatRe
-)
-
-type TextPattern struct {
-	Content   string
-	Re        *regexp.Regexp
-	Lowercase bool
-
-	TextPatternType
+// StringMatcher is an atomic pattern that could implement glob, literal or regex matchers
+type StringMatcher interface {
+	// StringMatch implements StringMatcher
+	StringMatch(string) bool
 }
 
-func (t TextPattern) Match(msg string) bool {
-	if t.Lowercase {
-		msg = strings.ToLower(msg)
-	}
-	switch t.TextPatternType {
-	case PatLiteral:
-		if strings.Contains(msg, t.Content) {
-			return true
-		}
-	case PatPrefix:
-		if strings.HasPrefix(msg, t.Content) {
-			return true
-		}
-	case PatSuffix:
-		if strings.HasSuffix(msg, t.Content) {
-			return true
-		}
-	case PatGlob:
-		if glob.Glob(t.Content, msg) {
-			return true
-		}
-	case PatRe:
-		if t.Re != nil && t.Re.MatchString(msg) {
+// StringMatchers holds multiple atomic matchers
+// Patterns are meant to be list of possibilities
+// thus, objects are joined with logical disjunctions
+type StringMatchers []StringMatcher
+
+// StringMatch implements StringMatcher
+func (s StringMatchers) StringMatch(msg string) bool {
+	for _, m := range s {
+		if m.StringMatch(msg) {
 			return true
 		}
 	}
 	return false
+}
+
+// ContentPattern is a token for literal content matching
+type ContentPattern struct {
+	Token     string
+	Lowercase bool
+}
+
+// StringMatch implements StringMatcher
+func (c ContentPattern) StringMatch(msg string) bool {
+	return strings.Contains(msg, func() string {
+		if c.Lowercase {
+			return strings.ToLower(c.Token)
+		}
+		return c.Token
+	}())
+}
+
+// PrefixPattern is a token for literal content matching
+type PrefixPattern struct {
+	Token     string
+	Lowercase bool
+}
+
+// StringMatch implements StringMatcher
+func (c PrefixPattern) StringMatch(msg string) bool {
+	return strings.HasPrefix(msg, func() string {
+		if c.Lowercase {
+			return strings.ToLower(c.Token)
+		}
+		return c.Token
+	}())
+}
+
+// SuffixPattern is a token for literal content matching
+type SuffixPattern struct {
+	Token     string
+	Lowercase bool
+}
+
+// StringMatch implements StringMatcher
+func (c SuffixPattern) StringMatch(msg string) bool {
+	return strings.HasSuffix(msg, func() string {
+		if c.Lowercase {
+			return strings.ToLower(c.Token)
+		}
+		return c.Token
+	}())
 }
