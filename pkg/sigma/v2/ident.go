d96449d9eb95489da10f9cf7eb50c534b7ce23e0
Markus Kont <markus.kont@ccdcoe.org>
2020-04-29 11:56:57 +0300

    WIP: Basic keyword can now match

--- pkg/sigma/v2/ident.go
+++ pkg/sigma/v2/ident.go
@@ -2,6 +2,7 @@ package sigma
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -56,12 +57,23 @@ func newRuleFromIdent(rule interface{}, kind identType) (Branch, error) {
 	return nil, fmt.Errorf("Unknown rule kind, should be keyword or selection")
 }
 
+// Keyword is a container for patterns joined by logical disjunction
 type Keyword struct {
+	S StringMatcher
 }
 
 // Match implements Matcher
 func (k Keyword) Match(msg Event) bool {
-	panic("not implemented") // TODO: Implement
+	msgs, ok := msg.Keywords()
+	if !ok {
+		return false
+	}
+	for _, m := range msgs {
+		if k.S.StringMatch(m) {
+			return true
+		}
+	}
+	return false
 }
 
 func newKeyword(expr interface{}) (*Keyword, error) {
@@ -69,7 +81,28 @@ func newKeyword(expr interface{}) (*Keyword, error) {
 	case []string:
 		return newStringKeyword(TextPatternContains, false, val...)
 	case []interface{}:
-		panic("TODO - keyword interface slice")
+		k, ok := isSameKind(val)
+		if !ok {
+			return nil, ErrInvalidKind{
+				Kind:     reflect.Array,
+				T:        identKeyword,
+				Critical: false,
+				Msg:      "Mixed type slice",
+			}
+		}
+		switch v := k; {
+		case v == reflect.String:
+			s, _ := castIfaceToString(val...)
+			return newStringKeyword(TextPatternContains, false, s...)
+		default:
+			return nil, ErrInvalidKind{
+				Kind:     v,
+				T:        identKeyword,
+				Critical: false,
+				Msg:      "Unsupported data type",
+			}
+		}
+
 	default:
 		// TODO
 		return nil, ErrInvalidKeywordConstruct{Expr: expr}
@@ -83,9 +116,13 @@ func newStringKeyword(mod TextPatternModifier, lower bool, patterns ...string) (
 	matcher := make(StringMatchers, 0)
 	for _, p := range patterns {
 		if strings.Contains(p, "*") {
-
+			return nil, ErrUnsupportedExpression{
+				Msg: "glob", T: identKeyword, Expr: patterns, Critical: true,
+			}
 		} else if strings.HasPrefix(p, "/") && strings.HasSuffix(p, "/") {
-
+			return nil, ErrUnsupportedExpression{
+				Msg: "regex", T: identKeyword, Expr: patterns, Critical: true,
+			}
 		} else {
 			switch mod {
 			case TextPatternSuffix:
@@ -97,7 +134,13 @@ func newStringKeyword(mod TextPatternModifier, lower bool, patterns ...string) (
 			}
 		}
 	}
-	return nil, nil
+	return &Keyword{
+		S: func() StringMatcher {
+			if len(matcher) == 1 {
+				return matcher[0]
+			}
+			return matcher
+		}()}, nil
 }
 
 type selection struct{}
@@ -110,4 +153,32 @@ func (s selection) Keywords() ([]string, bool) {
 // Select implements Selector
 func (s selection) Select(_ string) (interface{}, bool) {
 	panic("not implemented") // TODO: Implement
+}
+
+func isSameKind(data []interface{}) (reflect.Kind, bool) {
+	var current, last reflect.Kind
+	for i, d := range data {
+		current = reflect.TypeOf(d).Kind()
+		if i > 0 {
+			if current != last {
+				return current, false
+			}
+		}
+		last = current
+	}
+	return current, true
+}
+
+// castIfaceToString assumes that kind check has already been done
+func castIfaceToString(items ...interface{}) ([]string, int) {
+	tx := make([]string, 0)
+	var failed int
+	for _, val := range items {
+		if s, ok := val.(string); ok {
+			tx = append(tx, s)
+		} else {
+			failed++
+		}
+	}
+	return tx, failed
 }
