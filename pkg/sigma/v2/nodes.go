055ce8a296d56cb8631bb7d99d7eb2f4a53a8f30
Markus Kont <markus.kont@ccdcoe.org>
2020-05-04 10:02:48 +0300

    Implement logic operators

---
+++ pkg/sigma/v2/nodes.go
@@ -0,0 +1,59 @@
+package sigma
+
+// NodeSimpleAnd is a list of matchers connected with logical conjunction
+type NodeSimpleAnd []Branch
+
+// Match implements Matcher
+func (n NodeSimpleAnd) Match(e Event) bool {
+	for _, b := range n {
+		if !b.Match(e) {
+			return false
+		}
+	}
+	return true
+}
+
+// NodeSimpleAnd is a list of matchers connected with logical disjunction
+type NodeSimpleOr []Branch
+
+// Match implements Matcher
+func (n NodeSimpleOr) Match(e Event) bool {
+	for _, b := range n {
+		if b.Match(e) {
+			return true
+		}
+	}
+	return false
+}
+
+// NodeNot negates a branch
+type NodeNot struct {
+	B Branch
+}
+
+// Match implements Matcher
+func (n NodeNot) Match(e Event) bool {
+	return !n.B.Match(e)
+}
+
+// NodeAnd is a two element node of a binary tree with Left and Right branches
+// connected via logical conjunction
+type NodeAnd struct {
+	L, R Branch
+}
+
+// Match implements Matcher
+func (n NodeAnd) Match(e Event) bool {
+	return n.L.Match(e) && n.R.Match(e)
+}
+
+// NodeAnd is a two element node of a binary tree with Left and Right branches
+// connected via logical disjunction
+type NodeOr struct {
+	L, R Branch
+}
+
+// Match implements Matcher
+func (n NodeOr) Match(e Event) bool {
+	return n.L.Match(e) || n.R.Match(e)
+}
