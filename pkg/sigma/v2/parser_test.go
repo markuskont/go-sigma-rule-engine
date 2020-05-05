531db0d6403637729c9f3d02f3d07eb288790385
Markus Kont <markus.kont@ccdcoe.org>
2020-05-05 13:37:07 +0300

    Better parser tests, OR case requires exploring multiple branches

--- pkg/sigma/v2/parser_test.go
+++ pkg/sigma/v2/parser_test.go
@@ -70,13 +70,71 @@ detection:
     CommandLine: "+R +H +S +A *.cui"
 `
 
+var detection3 = `
+detection:
+  condition: "(selection1 or selection2) and not selection3"
+  selection1:
+    Image:
+    - '*\schtasks.exe'
+    - '*\nslookup.exe'
+    - '*\certutil.exe'
+    - '*\bitsadmin.exe'
+    - '*\mshta.exe'
+  selection2:
+    ParentImage:
+    - '*\mshta.exe'
+    - '*\powershell.exe'
+    - '*\cmd.exe'
+    - '*\rundll32.exe'
+    - '*\cscript.exe'
+    - '*\wscript.exe'
+    - '*\wmiprvse.exe'
+  selection3:
+    CommandLine: "+R +H +S +A *.cui"
+`
+
+var detection3_positive1 = `
+{
+	"Image":       "C:\\test\\bitsadmin.exe",
+	"CommandLine": "+R +H +A asd.cui",
+	"ParentImage": "C:\\test\\custom.exe",
+	"Image":       "C:\\test\\bitsadmin.exe",
+	"CommandLine": "aaa",
+	"ParentImage": "C:\\test\\wmiprvse.exe"
+}
+`
+var detection3_positive2 = `
+{
+	"Image":       "C:\\test\\custom.exe",
+	"CommandLine": "+R +H +A asd.cui",
+	"ParentImage": "C:\\test\\wmiprvse.exe",
+	"Image":       "C:\\test\\bitsadmin.exe",
+	"CommandLine": "aaa",
+	"ParentImage": "C:\\test\\wmiprvse.exe"
+}
+`
+
+var detection3_negative = `
+{
+	"Image":       "C:\\test\\bitsadmin.exe",
+	"CommandLine": "+R +H +S +A lll.cui",
+	"ParentImage": "C:\\test\\mshta.exe"
+}
+`
+
 type parseTestCase struct {
-	Rule, Pos, Neg string
+	Rule     string
+	Pos, Neg []string
 }
 
 var parseTestCases = []parseTestCase{
-	{Rule: detection1, Pos: detection1_positive, Neg: detection1_negative},
-	{Rule: detection2, Pos: detection1_positive, Neg: detection1_negative},
+	{Rule: detection1, Pos: []string{detection1_positive}, Neg: []string{detection1_negative}},
+	{Rule: detection2, Pos: []string{detection1_positive}, Neg: []string{detection1_negative}},
+	{
+		Rule: detection3,
+		Pos:  []string{detection3_positive1, detection3_positive2},
+		Neg:  []string{detection3_negative},
+	},
 }
 
 func TestTokenCollect(t *testing.T) {
@@ -116,19 +174,24 @@ func TestParse(t *testing.T) {
 				t.Fatalf("rule parser case %d failed to parse lexical tokens, %s", i+1, err)
 			}
 		}
-		var pos, neg DynamicMap
-
-		if err := json.Unmarshal([]byte(c.Pos), &pos); err != nil {
-			t.Fatalf("rule parser case %d positive case json unmarshal error %s", i+1, err)
+		var obj DynamicMap
+		// Positive cases
+		for _, c := range c.Pos {
+			if err := json.Unmarshal([]byte(c), &obj); err != nil {
+				t.Fatalf("rule parser case %d positive case json unmarshal error %s", i+1, err)
+			}
+			if !p.result.Match(obj) {
+				t.Fatalf("rule parser case %d positive case did not match", i+1)
+			}
 		}
-		if err := json.Unmarshal([]byte(c.Neg), &neg); err != nil {
-			t.Fatalf("rule parser case %d positive case json unmarshal error %s", i+1, err)
-		}
-		if !p.result.Match(pos) {
-			t.Fatalf("rule parser case %d positive case did not match", i+1)
-		}
-		if p.result.Match(neg) {
-			t.Fatalf("rule parser case %d negative case matched", i+1)
+		// Negative cases
+		for _, c := range c.Neg {
+			if err := json.Unmarshal([]byte(c), &obj); err != nil {
+				t.Fatalf("rule parser case %d positive case json unmarshal error %s", i+1, err)
+			}
+			if p.result.Match(obj) {
+				t.Fatalf("rule parser case %d negative case matched", i+1)
+			}
 		}
 	}
 }
